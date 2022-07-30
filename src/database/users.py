c36552eb361134861f69f7db5f64c6bd5892c1ee
jlegere45 <jlegere45@gmail.com>
2022-07-30 13:04:41 -0600

    Created template to update/get from users_table

---
+++ src/database/users.py
@@ -0,0 +1,24 @@
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+def insert_user():
+    pass
+
+
+def set_content_creator():
+    pass
+
+
+def search_for_user():
+    pass
+
+
+def get_user():
+    pass
+
+def set_session_token():
+    pass
+
+def register_email():
+    pass
