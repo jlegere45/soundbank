3ece665a9df8f13f9571c630f85b729c0784a9c7
jlegere45 <jlegere45@gmail.com>
2022-07-30 15:02:02 -0600

    User insert, get and delete functions written

--- src/database/users.py
+++ src/database/users.py
@@ -1,24 +1,53 @@
-from dotenv import dotenv_values
+"""user_table actions this is just a small collection of functions
+   caller needs to handle errors"""
+from src.database.db_connect import setup_connection
 
-config = dotenv_values(".env")
+conn, curr = setup_connection()
 
-def insert_user():
-    pass
+def insert_user(username, email, content_name=None, content_url=None,
+                content_about=None):
+    curr.execute("INSERT INTO users_table (username, email, registered) \
+                 VALUES (%s, %s, %s) RETURNING id, username, email",
+                (username, email, False))
+    result = curr.fetchone()
+    conn.commit()
+    return result
 
 
 def set_content_creator():
-    pass
-
+    curr.close()
+    conn.close()
 
 def search_for_user():
-    pass
+    curr.close()
+    conn.close()
 
+def get_user(id=None, username=None, email=None):
+    if id != None:
+        curr.execute("SELECT * FROM users_table WHERE id = %s", (id,))
+    elif username != None:
+        curr.execute("SELECT * FROM users_table WHERE username = %s", (username,))
+    elif email != None:
+        curr.execute("SELECT * FROM users_table WHERE email = %s", (email,))
+    else:
+        return None
+    return curr.fetchone()
 
-def get_user():
-    pass
 
 def set_session_token():
-    pass
-
+    curr.close()
+    conn.close()
+    
+    
 def register_email():
-    pass
+    curr.close()
+    conn.close()
+    
+    
+def delete_user(username=None, email=None, id=None):
+    if id == None:
+        curr.execute("DELETE FROM users_table WHERE username = %s and email = %s", (username, email))
+    else:
+        curr.execute("DELETE FROM users_table WHERE id = %s", (id,))
+    result = conn.commit()
+    return result
---
+++ tests/database/test_user_actions.py
@@ -0,0 +1,25 @@
+import pytest
+from src.database import users
+
+def test_insert_user(username1, email1):
+    result = users.insert_user(username1, email1)
+    assert result[0] > 1
+    assert result[1] == username1
+    assert result[2] == email1
+
+def test_get_user(username2, email2, test_user):
+    result = users.get_user(id=test_user)
+    assert result[0] == test_user
+    assert result[1] == username2
+    assert result[2] == email2
+    result = users.get_user(username=username2)
+    assert result[0] == test_user
+    assert result[1] == username2
+    assert result[2] == email2
+    result = users.get_user(email=email2)
+    assert result[0] == test_user
+    assert result[1] == username2
+    assert result[2] == email2
+
+def test_delete_user(username1, email1):
+    users.delete_user(username=username1, email=email1)
